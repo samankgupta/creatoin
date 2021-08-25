df049d2a5ac35eab0a221f182efea0e464931b3b
Samank Gupta <samankgupta@gmail.com>
2021-08-25 19:54:19 +0530

    Homepage frontend

---
+++ src/utils/Creators.js
@@ -0,0 +1,40 @@
+const creators = [
+    {
+        'name': 'PewDiePie',
+        'youtube': "https://www.youtube.com/user/pewdiepie",
+        'image': "https://www.biography.com/.image/t_share/MTUxNzIxODgzNTg5NDIwMzAw/pewdiepie_gettyimages-501661286.jpg",
+        'description': "Felix Arvid Ulf Kjellberg, better known online as PewDiePie, is a Swedish YouTuber known primarily for his Let's Play videos and comedic formatted videos and shows. Kjellberg's popularity on YouTube and extensive media coverage have made him one of the most noted online personalities and content creators.",
+    },
+    {
+        'name': 'MrBeast',
+        'youtube': "https://www.youtube.com/user/mrbeast6000",
+        'image': "https://pbs.twimg.com/profile_images/994592419705274369/RLplF55e.jpg",
+        'description': "Jimmy Donaldson, better known online as MrBeast, is an American YouTuber, businessman, and philanthropist. He has been credited with pioneering a genre of YouTube videos that center on expensive stunts.",
+    },
+    {
+        'name': 'Justin Bieber',
+        'youtube': "https://www.youtube.com/user/kidrauhl",
+        'image': "https://www.biography.com/.image/t_share/MTM2OTI2NTY2Mjg5NTE2MTI5/justin_bieber_2015_photo_courtesy_dfree_shutterstock_348418241_croppedjpg.jpg",
+        'description': "Justin Drew Bieber is a Canadian singer. He was discovered by American record executive Scooter Braun and signed with RBMG Records in 2008, gaining recognition with the release of his debut seven-track EP My World and soon establishing himself as a teen idol.",
+    },
+    {
+        'name': 'Ariana Grande',
+        'youtube': "https://www.youtube.com/user/osnapitzari",
+        'image': "https://www.biography.com/.image/t_share/MTQ3MzM3MTcxNjA5NTkzNjQ3/ariana_grande_photo_jon_kopaloff_getty_images_465687098.jpg",
+        'description': "Ariana Grande-Butera is an American singer and actress. She has received numerous accolades throughout her career, including two Grammy Awards, one Brit Award, two Billboard Music Awards, three American Music Awards, nine MTV Video Music Awards, and 27 Guinness World Records.",
+    },
+    {
+        'name': 'Eminem',
+        'youtube': "https://www.youtube.com/user/EminemMusic",
+        'image': "https://www.biography.com/.image/t_share/MTQ3NjM5MTEzMTc5MjEwODI2/eminem_photo_by_dave_j_hogan_getty_images_entertainment_getty_187596325.jpg",
+        'description': "Marshall Bruce Mathers III, known professionally as Eminem, is an American rapper, songwriter, and record producer. Eminem is among the best-selling music artists of all time, with estimated worldwide sales of over 220 million records.",
+    },
+    {
+        'name': 'Ed Sheeran',
+        'youtube': "https://www.youtube.com/user/EdSheeran",
+        'image': "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTM5ODkxNzYyODU1NDIwOTM4/ed-sheeran-gettyimages-494227430_1600jpg.jpg",
+        'description': "Edward Christopher Sheeran MBE is an English singer-songwriter, record producer, musician, and actor. Sheeran has sold more than 150 million records worldwide, making him one of the world's best-selling music artists.",
+    },
+]
+
+export default creators
